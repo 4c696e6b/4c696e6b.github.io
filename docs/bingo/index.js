34f6293890ae8845f9cdc410693e17d1e6162f2e
Link <4c696e6b@gmail.com>
2022-04-24 16:58:16 -0400

    cleaned up label array

--- docs/bingo/index.js
+++ docs/bingo/index.js
@@ -1,13 +1,35 @@
 function getLine () {
-    const lines = ['Animated Movie', 'Hard R in the movie', "Dong's Rec", 'Time Left Shown Accidentally',
-            'Boobas', 'Non-English Film', 'Horror/Gore', 'Arguing Mid Movie', 'WRONG CHANNEL',
-            'Grammar wants to play Rocket League', 'Bad CGI/Green Screen', 'AD Placement',
-            'Bad Sex Scene', 'Sci-Fi', 'Cam talks the whole movie', '~gay~',
-            'Someone rhymes something with Pussy', 'Potty Break', 'Mad goes on Deafen', 'Action Movie',
-            'ASS', 'Someone spoils the movie', 'Amp Arrives', 'Hydro Homie on Screen',
-            'Brooke forgets to start the event', 'Documentary',
-            "Aliens when there really doesn't need to be aliens", 'Audience Stand-In',
-            'Major MCU Cameo'];
+    const lines = [
+        'Animated Movie',
+        'Hard R in the movie',
+        "Dong's Rec",
+        'Time Left Shown Accidentally',
+        'Boobas',
+        'Non-English Film',
+        'Horror/Gore',
+        'Arguing Mid Movie',
+        'WRONG CHANNEL',
+        'Grammar wants to play Rocket League',
+        'Bad CGI/Green Screen',
+        'AD Placement',
+        'Bad Sex Scene',
+        'Sci-Fi',
+        'Cam talks the whole movie',
+        '~gay~',
+        'Someone rhymes something with Pussy',
+        'Potty Break',
+        'Mad goes on Deafen',
+        'Action Movie',
+        'ASS',
+        'Someone spoils the movie',
+        'Amp Arrives',
+        'Hydro Homie on Screen',
+        'Brooke forgets to start the event',
+        'Documentary',
+        "Aliens when there really doesn't need to be aliens",
+        'Audience Stand-In',
+        'Major MCU Cameo'
+        ];
 
     var r = Math.floor(Math.random() * (lines.length - 1));
     return lines[r];
