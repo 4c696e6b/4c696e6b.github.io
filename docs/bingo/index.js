152c57b809162190115f5d99d5f545cbe648bd20
Link <4c696e6b@gmail.com>
2022-11-02 17:36:52 -0400

    changed the bingo card generation code to not be ass

--- docs/bingo/index.js
+++ docs/bingo/index.js
@@ -1,70 +1,67 @@
-function getLine () {
-    const lines = [
-        '~gay~',
-        '1+ Year Time Skip',
-        'Action Movie',
-        'AD Placement',
-        'Animated Movie',
-        'Arguing Mid Movie',
-        'ASS',
-        'Audience Stand-In',
-        'Audio Balance Go Brrrrrr',
-        'Bad CGI/Green Screen',
-        'Bad Sex Scene',
-        'Blatant Racism',
-        'Boobas',
-        'Cheap jumpscare',
-        'Documentary/ Based on a True Story',
-        'DOG',
-        'Fr*nch :/',
-        'Homophobia',
-        'Horror/Gore',
-        'Hydro Homie on Screen',
-        "Link's stream does the thing",
-        'Main character accidentally murders a side character',
-        'Main character has dead parents',
-        'Major MCU Cameo',
-        'Misogyny!!!!',
-        'Non-English Film',
-        'Plot Armor saves lives',
-        'Roll Credits',
-        'Sci-Fi',
-        'SKRRT',
-        'Someone spoils the movie',
-        'Straight People be Like',
-        'The subtitles are ridiculous',
-        'Time Left Shown Accidentally',
-        'Toes >:/',
-        'Wildly Incorrect Science',
-        'Who Asked??????'
-        ];
+const lines = [
+    '~gay~',
+    '1+ Year Time Skip',
+    'Action Movie',
+    'AD Placement',
+    'Animated Movie',
+    'Arguing Mid Movie',
+    'ASS',
+    'Audience Stand-In',
+    'Audio Balance Go Brrrrrr',
+    'Bad CGI/Green Screen',
+    'Bad Sex Scene',
+    'Blatant Racism',
+    'Boobas',
+    'Cheap jumpscare',
+    'Documentary/ Based on a True Story',
+    'DOG',
+    'Fr*nch :/',
+    'Homophobia',
+    'Horror/Gore',
+    'Hydro Homie on Screen',
+    "Link's stream does the thing",
+    'Main character accidentally murders a side character',
+    'Main character has dead parents',
+    'Major MCU Cameo',
+    'Misogyny!!!!',
+    'Non-English Film',
+    'Plot Armor saves lives',
+    'Roll Credits',
+    'Sci-Fi',
+    'SKRRT',
+    'Someone spoils the movie',
+    'Straight People be Like',
+    'The subtitles are ridiculous',
+    'Time Left Shown Accidentally',
+    'Toes >:/',
+    'Wildly Incorrect Science',
+    'Who Asked??????'
+    ];
 
-    var r = Math.floor(Math.random() * lines.length);
-    return lines[r];
-}
 
-const usedLines = [];
+var numOfSquares = 0;
+var len = lines.length;
+var r;
+
 for (var i = 0; i < 24; i++) {
 
- 
-    var same = true;
-    while (same) {
-        
-        var line = getLine();
-        same = false;
+    // generate a random number based on the length of the array
+    r = Math.floor(Math.random() * len);
+    line = lines[r];
+    
+    // swap the chosen line and the last line in the array
+    lines[r] = lines[len-1];
+    lines[len-1] = line;
 
-        for (var j = 0; j < usedLines.length; j++) {
-            if (usedLines[j] == line) same = true;
-            }
-    }
-
-    usedLines.push(line);
+    // get the current square id and assign its value
     var id = "b" + String(i+1);
     document.getElementById(id).value = line;
+    
+    len -= 1;
 }
 
+// set the middle square
 document.getElementById('middle').value = ":) WHEEL SPIN :)";
-alert(temp);
 
-
+// function that allows the squares to be clicked
 function hlBtn() {$(this).toggleClass('hlBtn');}
---
+++ docs/bingo/old.js
@@ -0,0 +1,70 @@
+function getLine () {
+    const lines = [
+        '~gay~',
+        '1+ Year Time Skip',
+        'Action Movie',
+        'AD Placement',
+        'Animated Movie',
+        'Arguing Mid Movie',
+        'ASS',
+        'Audience Stand-In',
+        'Audio Balance Go Brrrrrr',
+        'Bad CGI/Green Screen',
+        'Bad Sex Scene',
+        'Blatant Racism',
+        'Boobas',
+        'Cheap jumpscare',
+        'Documentary/ Based on a True Story',
+        'DOG',
+        'Fr*nch :/',
+        'Homophobia',
+        'Horror/Gore',
+        'Hydro Homie on Screen',
+        "Link's stream does the thing",
+        'Main character accidentally murders a side character',
+        'Main character has dead parents',
+        'Major MCU Cameo',
+        'Misogyny!!!!',
+        'Non-English Film',
+        'Plot Armor saves lives',
+        'Roll Credits',
+        'Sci-Fi',
+        'SKRRT',
+        'Someone spoils the movie',
+        'Straight People be Like',
+        'The subtitles are ridiculous',
+        'Time Left Shown Accidentally',
+        'Toes >:/',
+        'Wildly Incorrect Science',
+        'Who Asked??????'
+        ];
+
+    var r = Math.floor(Math.random() * lines.length);
+    return lines[r];
+}
+
+const usedLines = [];
+for (var i = 0; i < 24; i++) {
+
+ 
+    var same = true;
+    while (same) {
+        
+        var line = getLine();
+        same = false;
+
+        for (var j = 0; j < usedLines.length; j++) {
+            if (usedLines[j] == line) same = true;
+            }
+    }
+
+    usedLines.push(line);
+    var id = "b" + String(i+1);
+    document.getElementById(id).value = line;
+}
+
+document.getElementById('middle').value = ":) WHEEL SPIN :)";
+alert(temp);
+
+
+function hlBtn() {$(this).toggleClass('hlBtn');}
